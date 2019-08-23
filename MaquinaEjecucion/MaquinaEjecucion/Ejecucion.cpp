40aa39f8991d0a37e149310f35a238844c1eb8f2
Luis <0199230@up.edu.mx>
2019-08-23 14:05:57 -0500

    base cases

--- MaquinaEjecucion/MaquinaEjecucion/Ejecucion.cpp
+++ MaquinaEjecucion/MaquinaEjecucion/Ejecucion.cpp
@@ -18,6 +18,53 @@ void Ejecucion::EjecutarInstruccion() {
 		cin >> reg[archivo.matrizCodificada[reg[7]].p1];
 		cout << "Guardado"<< endl;
 	}
+	case 2: {
+
+	}
+	case 3: {
+
+	}
+	case 4: {
+
+	}
+	case 5: {
+
+	}
+	case 6: {
+
+	}
+	case 7: {
+
+	}
+	case 8: {
+
+	}
+	case 9: {
+
+	}
+	case 10: {
+
+	}
+	case 11: {
+
+	}
+	case 12: {
+
+	}
+	case 13: {
+
+	}
+	case 14: {
+
+	}
+	case 15: {
+
+	}
+	case 16: {
+
+	}
+			 			 
+
 	}
 	reg[7]++;
 }
